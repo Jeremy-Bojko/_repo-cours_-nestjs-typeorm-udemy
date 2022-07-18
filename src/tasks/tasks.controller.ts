54742d552d5c1f076082b2de1c94254240b8b722
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-07-18 18:40:13 +0200

    16 - refactoring

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,75 +11,75 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getFilteredTasks(filterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
-  }
-
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
-
-  /**
-   * Version 1
-   */
-  // @Post()
-  // createTask(@Body() body) {
-  //   console.log(body);
+  // @Get()
+  // getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     return this.tasksService.getFilteredTasks(filterDto);
+  //   } else {
+  //     return this.tasksService.getAllTasks();
+  //   }
   // }
 
-  /**
-   * Version 2
-   */
-  // @Post()
-  // createTask(
-  //   @Body('title') title: string,
-  //   @Body('description') description: string,
-  // ): Task {
-  //   return this.tasksService.createTask(title, description);
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.tasksService.getTaskById(id);
   // }
 
-  /**
-   * Version 3
-   */
+  // /**
+  //  * Version 1
+  //  */
+  // // @Post()
+  // // createTask(@Body() body) {
+  // //   console.log(body);
+  // // }
+
+  // /**
+  //  * Version 2
+  //  */
+  // // @Post()
+  // // createTask(
+  // //   @Body('title') title: string,
+  // //   @Body('description') description: string,
+  // // ): Task {
+  // //   return this.tasksService.createTask(title, description);
+  // // }
+
+  // /**
+  //  * Version 3
+  //  */
+  // // @Post()
+  // // createTask(
+  // //   @Body() { title, description }: { title: string; description: string },
+  // // ): Task {
+  // //   return this.tasksService.createTask(title, description);
+  // // }
+
+  // /**
+  //  * Version 4
+  //  */
   // @Post()
-  // createTask(
-  //   @Body() { title, description }: { title: string; description: string },
-  // ): Task {
-  //   return this.tasksService.createTask(title, description);
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto);
   // }
 
-  /**
-   * Version 4
-   */
-  @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
+  // @Delete('/:id')
+  // deleteTaskById(@Param('id') id: string): string {
+  //   return this.tasksService.deleteTask(id);
+  // }
 
-  @Delete('/:id')
-  deleteTaskById(@Param('id') id: string): string {
-    return this.tasksService.deleteTask(id);
-  }
-
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  ) {
-    const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ) {
+  //   const { status } = updateTaskStatusDto;
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,88 +1,70 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
-
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  getTaskById(id: string): Task {
-    // try to get task
-    const found = this.tasks.find((task) => task.id === id);
-
-    if (!found) {
-      // if not found, throw an error (404 not found)
-      throw new NotFoundException(`Task with the id ${id} not found`);
-    }
-
-    // otherwise, return the found task
-    return found;
-  }
-
-  getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter((task) => {
-        if (task.description.includes(search) || task.title.includes(search)) {
-          return true;
-        }
-        return false;
-      });
-    }
-
-    return tasks;
-  }
-
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-
-    return task;
-  }
-
-  deleteTask(id: string): string {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter((task) => task.id !== found.id);
-    return `Task with id ${id} deleted successfully`;
-  }
-
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    // let taskIndex = -1;
-    // const taskToUpdate: Task = this.tasks.find((task, index) => {
-    //   if (task.id === id) {
-    //     taskIndex = index;
-    //     return task;
-    //   }
-    // });
-    // const updatedTask: Task = {
-    //   ...taskToUpdate,
-    //   status,
-    // };
-
-    // this.tasks.splice(taskIndex, 1, updatedTask);
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task;
-  }
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
+  // getTaskById(id: string): Task {
+  //   // try to get task
+  //   const found = this.tasks.find((task) => task.id === id);
+  //   if (!found) {
+  //     // if not found, throw an error (404 not found)
+  //     throw new NotFoundException(`Task with the id ${id} not found`);
+  //   }
+  //   // otherwise, return the found task
+  //   return found;
+  // }
+  // getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
+  //   let tasks = this.getAllTasks();
+  //   if (status) {
+  //     tasks = tasks.filter((task) => task.status === status);
+  //   }
+  //   if (search) {
+  //     tasks = tasks.filter((task) => {
+  //       if (task.description.includes(search) || task.title.includes(search)) {
+  //         return true;
+  //       }
+  //       return false;
+  //     });
+  //   }
+  //   return tasks;
+  // }
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
+  // deleteTask(id: string): string {
+  //   const found = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter((task) => task.id !== found.id);
+  //   return `Task with id ${id} deleted successfully`;
+  // }
+  // updateTaskStatus(id: string, status: TaskStatus): Task {
+  //   // let taskIndex = -1;
+  //   // const taskToUpdate: Task = this.tasks.find((task, index) => {
+  //   //   if (task.id === id) {
+  //   //     taskIndex = index;
+  //   //     return task;
+  //   //   }
+  //   // });
+  //   // const updatedTask: Task = {
+  //   //   ...taskToUpdate,
+  //   //   status,
+  //   // };
+  //   // this.tasks.splice(taskIndex, 1, updatedTask);
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
