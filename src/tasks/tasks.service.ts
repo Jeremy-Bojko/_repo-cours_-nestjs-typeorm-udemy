938b70ead01595511270b67134ef52981e645a63
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-07-19 17:24:58 +0200

    18.1 - create task advanced

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -52,29 +52,9 @@ export class TasksService {
   //   return tasks;
   // }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const createdTask = this.taskRepository.create({
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    });
-
-    await this.taskRepository.save(createdTask);
-    return createdTask;
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto);
   }
-  // createTask(createTaskDto: CreateTaskDto): Task {
-  //   const { title, description } = createTaskDto;
-  //   const task: Task = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-  //   this.tasks.push(task);
-  //   return task;
-  // }
 
   // deleteTask(id: string): string {
   //   const found = this.getTaskById(id);
